ae49e9801ed07a86fdaf87a3608a58a90f8deb87
Tomszy <tothtomi007@gmail.com>
2022-11-28 18:13:22 +0100

    Splash Screen added to the app.

--- app/src/main/java/com/example/currencyconverterapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/SplashScreenActivity.kt
@@ -3,15 +3,28 @@ package com.example.currencyconverterapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import com.airbnb.lottie.LottieAnimationView
+import pl.droidsonroids.gif.GifImageView
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Thread.sleep(3000)
-        val i = Intent(this, MainActivity::class.java)
-        startActivity(i)
-        finish()
+        val astronautGif = findViewById<GifImageView>(R.id.gifView)
+        val textView = findViewById<TextView>(R.id.bestAppTextView)
+
+        astronautGif.animate().setDuration(5000).alpha(1f).withStartAction{
+            textView.animate().setDuration(5000).alpha(1f).
+            withEndAction{
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                overridePendingTransition(0, android.R.anim.fade_out)
+                finish()
+            }
+        }
     }
 }
